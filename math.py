578278a54662a255505665b329192560c62fff24
samik-8161 <samikshaa8161@gmail.com>
2021-06-16 15:03:00 +0530

    added fun headers

--- math.py
+++ math.py
@@ -1,12 +1,15 @@
 # Add implementation
 def add(x,y):
 	pass
-
+    
+# sub implementation
 def subtract(x,y):
 	pass
 
+# multiply implementation
 def multiply(x,y):
 	pass
 
+# divide implementation
 def divide(x,y):
 	pass
